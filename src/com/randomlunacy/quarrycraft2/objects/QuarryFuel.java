863518264787a8634bb3faa53c56bd24ed620fb0
Mike Garlick <mike@earthlingsnw.com>
2021-12-15 20:53:10 -0800

    Add additional fuels

--- src/com/randomlunacy/quarrycraft2/objects/QuarryFuel.java
+++ src/com/randomlunacy/quarrycraft2/objects/QuarryFuel.java
@@ -3,7 +3,7 @@ package com.randomlunacy.quarrycraft2.objects;
 import org.bukkit.Material;
 
 public enum QuarryFuel {
-    COAL(50f), COAL_BLOCK(450f), CHARCOAL(25f), REDSTONE(100f), REDSTONE_BLOCK(900f);
+    COAL(50f), COAL_BLOCK(450f), CHARCOAL(25f), REDSTONE(100f), REDSTONE_BLOCK(900f), BLAZE_POWDER(200f), BLAZE_ROD(400f), NETHER_STAR(20000f), ENDER_PEARL(900f);
 
     private float energyValue;
 
@@ -27,6 +27,14 @@ public enum QuarryFuel {
                 return QuarryFuel.REDSTONE;
             case REDSTONE_BLOCK:
                 return QuarryFuel.REDSTONE_BLOCK;
+            case BLAZE_POWDER:
+                return QuarryFuel.BLAZE_POWDER;
+            case BLAZE_ROD:
+                return QuarryFuel.BLAZE_ROD;
+            case NETHER_STAR:
+                return QuarryFuel.NETHER_STAR;
+            case ENDER_PEARL:
+                return QuarryFuel.ENDER_PEARL;
             default:
                 return null;
         }
